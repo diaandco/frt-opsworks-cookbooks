06331165995d28f0bdad5663c4c54b9e9e90895b
Jason Meredith <jmeredith@dia.co>
2018-12-12 16:18:25 -0500

    getting logs and permissions fixed

--- dd-agent-configs/recipes/default.rb
+++ dd-agent-configs/recipes/default.rb
@@ -1,4 +1,28 @@
+group 'nginx' do
+  action	:modify
+  members 	["dd-agent"]
+  append	true
+end
+
 if node['dd-agent-configs']['rails']['installed']
+    execute 'update_logrotate' do
+      user    'root'
+      command 'grep "create 664" /etc/logrotate.d/opsworks_app_diaandco || sed -i -e "s/\}/        create 644 deploy nginx\n\}/" /etc/logrotate.d/opsworks_app_diaandco'
+    end
+    execute 'chmod_shared_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared'
+    end
+    execute 'chmod_log_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared/log'
+    end
+    for log_file in node['dd-agent-configs']['rails']['logs'] do
+      file log_file['path'] do
+        action    :touch
+        mode	0644
+      end
+    end
     directory File.join(node['datadog']['agent6_config_dir'], 'conf.d/rails.d') do
         owner 'dd-agent'
         group 'dd-agent'
@@ -15,6 +39,24 @@ if node['dd-agent-configs']['rails']['installed']
 end
 
 if node['dd-agent-configs']['sidekiq']['installed']
+    execute 'update_logrotate' do
+      user    'root'
+      command 'grep "create 664" /etc/logrotate.d/opsworks_app_diaandco || sed -i -e "s/\}/        create 644 deploy nginx\n\}/" /etc/logrotate.d/opsworks_app_diaandco'
+    end
+    execute 'chmod_shared_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared'
+    end
+    execute 'chmod_log_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared/log'
+    end
+    for log_file in node['dd-agent-configs']['sidekiq']['logs'] do
+      file log_file['path'] do
+        action    :touch
+        mode	0644
+      end
+    end
     directory File.join(node['datadog']['agent6_config_dir'], 'conf.d/sidekiq.d') do
         owner 'dd-agent'
         group 'dd-agent'
@@ -31,6 +73,24 @@ if node['dd-agent-configs']['sidekiq']['installed']
 end
 
 if node['dd-agent-configs']['whenever']['installed']
+    execute 'update_logrotate' do
+      user    'root'
+      command 'grep "create 664" /etc/logrotate.d/opsworks_app_diaandco || sed -i -e "s/\}/        create 644 deploy nginx\n\}/" /etc/logrotate.d/opsworks_app_diaandco'
+    end
+    execute 'chmod_shared_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared'
+    end
+    execute 'chmod_log_dir' do
+      user    'root'
+      command 'chmod 771 /srv/www/diaandco/shared/log'
+    end
+    for log_file in node['dd-agent-configs']['whenever']['logs'] do
+      file log_file['path'] do
+        action    :touch
+        mode	0644
+      end
+    end
     directory File.join(node['datadog']['agent6_config_dir'], 'conf.d/whenever.d') do
         owner 'dd-agent'
         group 'dd-agent'
